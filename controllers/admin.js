d3a7580495dfff1238db7aa345eda18fa8d15d6c
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-05-17 13:53:51 +0530

    date filtering sales report

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,68 +1,88 @@
 const admin = require('../model/adminModel')
 const user = require('../model/userModel')
 const category = require('../model/category')
-const product=require('../model/product')
+const product = require('../model/product')
 const bcrypt = require('bcrypt')
-const order=require('../model/orders')
+const order = require('../model/orders')
 
 // //adminDashbord
-var ord=0;
-var del=0;
-var ret=0;
-var can=0;
-var shi=0;
-var dis=0;
+var ord = 0;
+var del = 0;
+var ret = 0;
+var can = 0;
+var shi = 0;
+var dis = 0;
 
 
-const adminDashboard = async(req, res) => {
+const adminDashboard = async (req, res) => {
     try {
-        const orderDatas=await order.find().lean()
-        console.log("orderDatas:",orderDatas)
-        const datas= orderDatas.map((item)=>{
+
+        const { from, to } = req.query;
+        console.log(req.query)
+
+        const fromDate = new Date(from);
+        const toDate = new Date(to);
+
+        // Increment the toDate by 1 day to include the entire last day
+        toDate.setDate(toDate.getDate() + 1);
+
+        // Format the dates in "dd mm yy" order
+        const fromFormatted = fromDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
+        const toFormatted = toDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
+
+        const orderData = await order.find({
+            date: {
+                $gte: fromFormatted,
+                $lt: toFormatted,
+            },
+        }).lean();
+        const orderDatas = await order.find().lean()
+        console.log("orderDatas:", orderDatas)
+        const datas = orderDatas.map((item) => {
             return item.status
         })
-        console.log("datas:",datas)
+        console.log("datas:", datas)
         let ord = datas.filter(status => status === 'ordered').length;
-        let del= datas.filter(status => status === 'delivered').length;
+        let del = datas.filter(status => status === 'delivered').length;
         let ret = datas.filter(status => status === 'returned').length;
         let can = datas.filter(status => status === 'Cancelled').length;
-        let shi= datas.filter(status => status === 'Shipped').length;
+        let shi = datas.filter(status => status === 'Shipped').length;
         let dis = datas.filter(status => status === 'dispatched').length;
-        
-        var mo=0;var tu=0;var we=0;var th=0; var fr=0; var sa=0; var su=0;
-        for (i=0;i<orderDatas.length;i++){
+
+        var mo = 0; var tu = 0; var we = 0; var th = 0; var fr = 0; var sa = 0; var su = 0;
+        for (i = 0; i < orderDatas.length; i++) {
             const date = new Date(orderDatas[i].date);
-                const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });;
-                console.log(dayOfWeek)
-                if(dayOfWeek.toUpperCase()=="MONDAY"){
+            const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });;
+            console.log(dayOfWeek)
+            if (dayOfWeek.toUpperCase() == "MONDAY") {
                 mo++;
-              }
-            if(dayOfWeek.toUpperCase()=="TUESDAY"){
+            }
+            if (dayOfWeek.toUpperCase() == "TUESDAY") {
                 tu++;
-              }
-            if(dayOfWeek.toUpperCase()=="WEDNESDAY"){
+            }
+            if (dayOfWeek.toUpperCase() == "WEDNESDAY") {
                 we++;
-              }
-            if(dayOfWeek.toUpperCase()=="THURSDAY"){
+            }
+            if (dayOfWeek.toUpperCase() == "THURSDAY") {
                 th++;
-              }
-            if(dayOfWeek.toUpperCase()=="FRIDAY"){
+            }
+            if (dayOfWeek.toUpperCase() == "FRIDAY") {
                 fr++;
-              }
-            if(dayOfWeek.toUpperCase()=="SATURDAY"){
+            }
+            if (dayOfWeek.toUpperCase() == "SATURDAY") {
                 sa++;
-              }
-            if(dayOfWeek.toUpperCase()=="SUNDAY"){
+            }
+            if (dayOfWeek.toUpperCase() == "SUNDAY") {
                 su++;
-              }
-    
+            }
+
         }
-        
-        res.render('adminside/dashboard', { admin: true, admin1: true,orderDatas,ord,del,ret,can,shi,dis,mo,tu,we,th,fr,sa,su})
+
+        res.render('adminside/dashboard', { admin: true, admin1: true, orderDatas, orderData,ord, del, ret, can, shi, dis, mo, tu, we, th, fr, sa, su })
     } catch (error) {
         console.log(error.message)
     }
-   
+
 }
 
 
@@ -82,7 +102,7 @@ const blockUser = async (req, res) => {
         const id = req.query.id;
         const userData1 = await user.updateOne({ _id: id }, { $set: { status: false } })
         // console.log("___"+userData1)
-        req.session.user=null;
+        req.session.user = null;
         res.redirect('/userList')
     } catch (error) {
         console.log(error.message)
@@ -107,7 +127,7 @@ const adminLogin = async (req, res) => {
         res.render('adminside/signin', { admin1: true })
 
     } catch (error) {
-       console.log(error.message)
+        console.log(error.message)
     }
 }
 
@@ -119,15 +139,15 @@ const postAdminLogin = async (req, res) => {
         if (findAdmin) {
             const passwordMatch = await bcrypt.compare(adminPassword, findAdmin.password)
             if (passwordMatch) {
-                req.session.admin=findAdmin._id
+                req.session.admin = findAdmin._id
                 res.redirect('/adminDashboard')
             } else {
-                res.render('adminside/signin', { admin1: true ,err:"invalid credentials"})
+                res.render('adminside/signin', { admin1: true, err: "invalid credentials" })
 
             }
         } else {
-            res.render('adminside/signin', { admin1: true ,err:"invalid credentials"})
-            
+            res.render('adminside/signin', { admin1: true, err: "invalid credentials" })
+
         }
 
     } catch (error) {
@@ -138,12 +158,12 @@ const postAdminLogin = async (req, res) => {
 
 // adminLogout
 
-const adminLogout=async(req,res)=>{
+const adminLogout = async (req, res) => {
     try {
         req.session.destroy()
         res.redirect('/admin')
     } catch (error) {
-console.log(error.message)
+        console.log(error.message)
     }
 }
 module.exports = {
